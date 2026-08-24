423d8bdbc06f159ad752d1ce5d12cd13a5f2e78c
hamad-sha <spiderfire197@gmail.com>
2026-08-24 13:51:31 +0500

    Add transaction handling and update README

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-Balance = 0
+balance = 0
+transactions = []
 def display_menu():
     print("\n--- Finance Tracker ---")
     print("1. Add income")
@@ -17,6 +18,27 @@ def choice_select():
                     print("Error: Number must be between 1 and 5.")
         except ValueError:
             print("Enter a valid number!")
-
+def display_balance():
+    print(f"Current balance: {balance}")
+def transaction():
+    global balance
+    transaction_amount = float(input("Enter your income/expense."))
+    balance = balance + transaction_amount
+    if transaction_amount > 0:
+        print(f"Your income inputted: {transaction_amount}")
+    else:
+        print(f"Your expense inputted: {transaction_amount}")
 display_menu()
+choice = choice_select()
+while choice != 5:
+    if choice in (1,2):
+        transaction()
+    elif choice == 3:
+        display_balance()
+    elif choice == 4:
+        display_transactions()
+    display_menu()
+    choice = choice_select()
+
+
 
