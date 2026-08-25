e4ad8a058bae4f24c00b686693d75d604ac1e3eb
hamad-sha <spiderfire197@gmail.com>
2026-08-25 20:39:32 +0500

    feat: separate deposit and withdrawal logic

--- main.py
+++ main.py
@@ -15,28 +15,38 @@ def choice_select():
         try:
             choice = int(input("Enter the number of your choice!"))
             match choice:
-                case 1|2|3|4|5:
+                case 1|2|3|4|5|6:
                     return choice
                 case _:
-                    print("Error: Number must be between 1 and 5.")
+                    print("Error: Number must be between 1 and 6.")
         except ValueError:
             print("Enter a valid number!")
 def display_balance():
     print(f"Current balance: {balance}")
     input()
-def transaction():
-    category = input("What is the category of your payment?")
-    description = input("Small description regarding your payment.")
+def deposit():
+    category = input("What is the category of your deposit?")
+    description = input("Small description regarding your deposit.")
     global balance
-    transaction_amount = float(input("Enter your income/expense."))
+    transaction_amount = float(input("Enter the amount of your deposit."))
     balance = balance + transaction_amount
-    if transaction_amount > 0:
-        print(f"Your income inputted: {transaction_amount}")
-    else:
-        print(f"Your expense inputted: {transaction_amount}")
+    print(f"You deposited: {transaction_amount}$")
     transaction = {
         "Category" : category,
         "Amount" : transaction_amount,
+        "Description" : description
+    }
+    transactions.append(transaction)
+def withdraw():
+    category = input("What is the category of your withdraw?")
+    description = input("Small description regarding your withdraw.")
+    global balance
+    transaction_amount = float(input("Enter the amount of your withdraw."))
+    balance = balance - transaction_amount
+    print(f"You withdrew: {transaction_amount}$")
+    transaction = {
+        "Category" : category,
+        "Amount" : transaction_amount * -1,
         "Description" : description
     }
     transactions.append(transaction)
@@ -45,7 +55,6 @@ def display_transactions():
         print("No transaction has occcured!")
         input()
     else:
-        counter = 0
         print(tabulate(transactions, headers="keys", tablefmt="grid"))
         input()
 def search_transaction():
@@ -79,8 +88,10 @@ def search_transaction():
 display_menu()
 choice = choice_select()
 while choice != 6:
-    if choice in (1,2):
-        transaction()
+    if choice == 1:
+        deposit()
+    elif choice == 2:
+        withdraw()
     elif choice == 3:
         display_balance()
     elif choice == 4:
